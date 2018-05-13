23264d6b722afc09ca2e78377115d7d05d13822e
sopmaq <scmaquiling@ucdavis.edu>
2018-05-13 16:27:48 -0700

    changed create label onclick function

--- js/script.js
+++ js/script.js
@@ -15,20 +15,16 @@ function createLabel() {
   labels.style.display = 'flex';
 
   var addLabelFunc = function addLabel() {
-    console.log('addLabelFunc')
-    var div = document.createElement('div');
-    labels.appendChild(div);
     var value = document.getElementById('label-name').value;
-    div.textContent = value;
     var color = document.getElementById('colorpicker').value;
-    div.style.backgroundColor = color;
-    div.style.color = 'white';
     var newLabel = {
         name: value,
         color: color
     }
     labelArray.push(newLabel);
     console.log(labelArray)
+    closeOverlay();
+    updateLabels(newLabel);
   }; // addLabel()
 
   var submit = document.getElementById('labelSubmit');
@@ -40,20 +36,17 @@ function showOverlay() {
 }
 
 function closeOverlay()  {
+  document.getElementById('background-overlay').style.display = 'none';
   document.querySelectorAll('.overlay').forEach(function(overlay){
     overlay.style.display = 'none'
   });
-  document.getElementById('background-overlay').style.display = 'none';
-  updateLabels();
 } // closeOverlay
 
 
-function updateLabels() {
+function updateLabels(newLabel) {
   var labelList = document.getElementById('labelList');
-  for (i in labelArray) {
-    var div = document.createElement('div');
-    labelList.appendChild(div);
-    div.textContent = labelArray[i].name;
-    div.style.backgroundColor = labelArray[i].color;
-  }
+  var div = document.createElement('div');
+  labelList.appendChild(div);
+  div.textContent = newLabel.name;
+  div.style.backgroundColor = newLabel.color;
 }; // updateLabels
